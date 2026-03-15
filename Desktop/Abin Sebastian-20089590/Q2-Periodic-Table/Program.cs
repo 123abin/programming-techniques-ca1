52892b41c12888d683df4a9af437d9e28b50cf8c
Abin sebastian <7abinsebastian@gmail.com>
2026-03-15 20:11:53 +0000

    Periodic table program

---
+++ Desktop/Abin Sebastian-20089590/Q2-Periodic-Table/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Periodic Table Program");
+
+        Dictionary<int, string> elements = new Dictionary<int, string>();
+
+        elements.Add(1, "Hydrogen - Nonmetal");
+        elements.Add(2, "Helium - Noble Gas");
+        elements.Add(3, "Lithium - Alkali Metal");
+        elements.Add(4, "Beryllium - Alkaline Earth Metal");
+        elements.Add(5, "Boron - Metalloid");
+
+        Console.Write("Enter atomic number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (elements.ContainsKey(number))
+        {
+            Console.WriteLine("Atomic Number: " + number);
+            Console.WriteLine("Element: " + elements[number]);
+        }
+        else
+        {
+            Console.WriteLine("Element not found.");
+        }
+    }
+}
