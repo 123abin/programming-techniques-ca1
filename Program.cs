113a0efbb189c1479962cf15cba8407df49d556b
Abin sebastian <7abinsebastian@gmail.com>
2026-03-17 11:42:19 +0000

    Implemented menu for churros system

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static int orderNumber = 1;
+
+    static void Main()
+    {
+        int choice;
+
+        do
+        {
+            Console.WriteLine("\n--- Delicious Churros ---");
+            Console.WriteLine("1. Place Order");
+            Console.WriteLine("2. Deliver Order");
+            Console.WriteLine("0. Exit");
+            Console.Write("Enter your choice: ");
+
+            choice = Convert.ToInt32(Console.ReadLine());
+
+        } while (choice != 0);
+    }
+}
