932dd85ccceb8fb41dddf35ceef55356918b097d
Abin sebastian <7abinsebastian@gmail.com>
2026-03-22 12:55:15 +0000

    started churros system with basic structure

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static Queue<Order> orderQueue = new Queue<Order>();
+    static int orderNumber = 1;
+
+    static void Main()
+    {
+        Console.WriteLine("Churros System Started");
+    }
+}
+
+public class Order
+{
+}
