d5f3c5865a3824e4887657a61ccc70ee6ede1c23
Abin sebastian <7abinsebastian@gmail.com>
2026-03-26 14:02:05 +0000

    Added element types

--- Question 2/Program.cs
+++ Question 2/Program.cs
@@ -5,19 +5,19 @@ class Program
 {
     static void Main()
     {
-        Dictionary<int, string> elements = new Dictionary<int, string>();
-
-        elements.Add(1, "Hydrogen");
-        elements.Add(2, "Helium");
-        elements.Add(3, "Lithium");
-        elements.Add(4, "Beryllium");
-        elements.Add(5, "Boron");
+        Dictionary<int, (string Name, string Type)> elements = new Dictionary<int, (string, string)>();
+        
+        elements.Add(1, ("Hydrogen", "Nonmetal"));
+        elements.Add(2, ("Helium", "Noble gas"));
+        elements.Add(3, ("Lithium", "Alkali metal"));
+        elements.Add(4, ("Beryllium", "Alkaline earth metal"));
+        elements.Add(5, ("Boron", "Metalloid"));
 
         Console.WriteLine("Elements List:");
 
         foreach (var element in elements)
-        {
-            Console.WriteLine(element.Key + " - " + element.Value);
-        }
+{
+    Console.WriteLine(element.Key + " - " + element.Value.Name + " (" + element.Value.Type + ")");
+}
     }
 }
