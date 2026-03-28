cbe6a35da90f451fb3dbd96d1fe926a17f3f3a45
Abin sebastian <7abinsebastian@gmail.com>
2026-03-28 20:00:27 +0000

    Data stored in the database

--- Question 3/Que3-server.py
+++ Question 3/Que3-server.py
@@ -1,6 +1,14 @@
 import socket
+import sqlite3
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn_db = sqlite3.connect("customers.db")
+cursor = conn_db.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS customers (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT ,address TEXT ,pps TEXT , license TEXT )""")
+                
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
 server.bind(("localhost", 12345))
 server.listen(1)
 
@@ -12,4 +20,12 @@ print("Connected by", addr)
 data = conn.recv(1024).decode()
 print("Received:", data)
 
+name, address, pps, license = data.split(",")
+
+cursor.execute("INSERT INTO customers (name, address, pps, license) VALUES (?, ?, ?, ?)", (name, address, pps, license))
+conn_db.commit()
+
+print("Data stored in the database!")
+
 conn.close()
+conn_db.close()
