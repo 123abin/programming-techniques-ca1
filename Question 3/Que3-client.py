c63a21cd8861678bfaa9d3b58c1d236726d9ecb7
Abin sebastian <7abinsebastian@gmail.com>
2026-03-28 19:32:44 +0000

    Client sends customer details

--- Question 3/Que3-client.py
+++ Question 3/Que3-client.py
@@ -3,6 +3,12 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 12345))
 
-client.send("hello".encode())
+name = input("Enter Name: ")
+address = input("Enter Address: ")
+pps = input("Enter PPS Number: ")
+license = input("Enter Driving License: ")
 
+data = f"{name},{address},{pps},{license}"
+
+client.send(data.encode())
 client.close()
